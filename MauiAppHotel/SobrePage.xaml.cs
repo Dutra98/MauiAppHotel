7bcb44a2a088aa710d837bec9290e2a69f50bcb4
Thiago Dutra <thiagoodelima@gmail.com>
2025-05-25 18:49:26 -0300

    Adicionados recursos ao aplicativo

---
+++ MauiAppHotel/SobrePage.xaml.cs
@@ -0,0 +1,13 @@
+namespace MauiAppHotel;
+
+public partial class SobrePage : ContentPage
+{
+	public SobrePage()
+	{
+		InitializeComponent();
+	}
+    private async void OnVoltarClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+}
